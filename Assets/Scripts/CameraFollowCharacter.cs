1dcf4a15cf9fb837ec44ca4dcf880af6950e909e
Hugey <tylerhugenberg@gmail.com>
2015-03-28 17:01:06 -0400

    Added tons of assets. Camera is following player waist

---
+++ Assets/Scripts/CameraFollowCharacter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowCharacter : MonoBehaviour {
+
+	public Transform player;
+
+	void Update () {
+		transform.position = new Vector3 (player.position.x, player.position.y, this.transform.position.z);
+	}
+}
